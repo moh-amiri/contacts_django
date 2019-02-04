2b52cbc33a08f370febbfd256f877931cfb83f23
aaamir829 <aaamir829@gmail.com>
2019-02-04 15:55:30 +0330

    تغییرات پوشه testFiles

--- testFiles/classes.py
+++ testFiles/classes.py
@@ -1,6 +1,13 @@
 class Dog():
-    def __init__(self,breed):
+    # attribute of class
+    attOne='oneAtt'
+    def __init__(self,breed,poww):
         self.breed=breed
+        self.poww=poww
 
-objectDog=Dog(breed='something')
-print("Class of Dog()"+str(objectDog))        
+
+objectDog=Dog('something1','something2')
+anotherdog=Dog('one','two')
+# print("Class of Dog()"+str(objectDog))
+# print("Class of Dog():arg1=> "+str(objectDog.breed)+" arg2=> "+str(objectDog.poww))
+print('attOne:'+anotherdog.attOne+" Class of Dog():arg1=> "+str(anotherdog.breed)+" arg2=> "+str(anotherdog.poww))
